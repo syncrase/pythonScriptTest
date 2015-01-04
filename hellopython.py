3d2a26c3827003f301a3b3bdff9b2e2344ce6c76
pierre <pierre.taquet@gmail.com>
2015-01-04 20:11:32 +0100

    Ajout des templates de code supplementaire

--- hellopython.py
+++ hellopython.py
@@ -20,6 +20,7 @@ from mypackage.libperso import signatureTest
 #Si on ne veut pas réécrire systématiquement le nom du module, il faut importer la fonction (valable pour toutes les fontions)
 from mypackage.libperso import *
 		
+
 		
 ###############################################################################################
 #Affichage vers la sortie standard
@@ -29,7 +30,8 @@ from mypackage.libperso import *
 ###############################################################################################
 #Instanciation et initialisation des variables des différents types
 #utilisationVariables()
-
+#stringObject()
+#listObject()
 
 ###############################################################################################
 #Utilisation du flux standard de sortie
@@ -38,7 +40,7 @@ from mypackage.libperso import *
 	
 ###############################################################################################
 #boucleWhile()
-
+#boucleFor()
 
 ###############################################################################################
 #Exemple d'utilisation d'une fonction
@@ -68,7 +70,37 @@ from mypackage.libperso import *
 
 ###############################################################################################
 #assertionError()
+#exceptionRaised()
+#functionsExamples()
 
+
+###############################################################################################
+"""fonction_multiparametres()
+fonction_multiparametres(1)
+fonction_multiparametres('un',1,"hein!")
+"""
+"""
+fonction_multiparametres_avec_2_param_mini('taquet','pierre')
+fonction_multiparametres_avec_2_param_mini('taquet','pierre', 5, 2, 1, 'coucou')
+"""
+"""
+fonction_multiparametres_full('salut')#le param obligatoire
+fonction_multiparametres_full('salut',1,'\n')#le param obligatoire et d'autres
+list_param = ['salut',1,'\n']
+fonction_multiparametres_full(*list_param,pays='corée centrale', planete='jupiter')#le param obligatoire et d'autres. Plus les paramètres nommés, qui ont forcément une valeur par défaut
+"""
+
+###############################################################################################
+#comprehension_de_liste()
+
+#dico()
+#dico_de_fonctions()
+#fonction_multi_parametres_nommes(param1='Hello', grizzly='pepette')
+#fonction_jmenfoutiste('salut',2,True, [1,'2',3], {'key':'u8'} , dico={'chiffre':6}, entier = 2)
+
+###############################################################################################
+#fichiers()
+#sauvegarde_objet()
 
 # On met le programme en pause pour éviter qu'il ne se referme (Windows)
 os.system("pause")#Apparaitra "Appuyer sur une touche pour continuer..."
--- mypackage/libperso.py
+++ mypackage/libperso.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3.4
+# -*-coding:UTF-8 -*
 #Author : Pierre TAQUET
 #Description : This file contains fonctions to be called.
 #Creation date : 31/12/2014
@@ -13,6 +15,10 @@
 
 import os
 import math
+from random import randrange
+import pickle
+
+i=4#variable globale, appellée dans le code de la manière suivante : global i
 
 def table(nb, max=10):
 	i=0
@@ -45,8 +51,25 @@ if __name__ == "__main__":
 def affichageSTDIN():
 	print("Hello, Python!")
 	#Exemple sur plusieurs lignes
-	print("Hello, "+"""
-Python!""")#ici, la tabulation est considérée comme un caractère affiché
+	print("\tHello, "+"""
+	Python!""")#ici, la tabulation est considérée comme un caractère affiché
+	print("Bonjour {0} {1}, tu as {2} ans.".format("toto", "toto", "49"))
+	
+	#La méthode format
+	prenom = "Paul"
+	nom = "Dupont"
+	age = 21
+	print( \
+	"Je m'appelle {0} {1} ({3} {0} pour l'administration) et j'ai {2} " \
+	"ans.".format(prenom, nom, age, nom.upper()))
+	# formatage d'une adresse
+	adresse = """
+	{no_rue}, {nom_rue}
+	{code_postal} {nom_ville} ({pays})"""\
+	.format(no_rue=5, nom_rue="rue des Postes", code_postal=75003, nom_ville="Paris", pays="France")
+	print(adresse)
+
+
 	
 def utilisationVariables():
 	varInt = 8				#var is an int
@@ -72,6 +95,111 @@ def utilisationVariables():
 	else:
 		print("La variable est nulle")
 		
+	#Suppression des variables, celles-ci n'existerons plus en mémoire. Désallocation de la memoire.
+	del varInt
+	del varString
+	del varChar
+		
+		
+def stringObject():
+	chaine = str() # Crée une chaîne vide
+	# On aurait obtenu le même résultat en tapant chaine = ""
+	#prenom = str()
+	#nom = str()
+	#age = str()
+	while chaine.lower() != "k" and chaine.lower() != "qua":
+		print("Tapez 'K' ou 'k' ou 'qua'  ou 'qUa' ou 'qUA' ou 'QuA' ou ... pour kontinuer :-D ")
+		chaine = input()
+
+	print("Donne moi ton prénom stp. ")
+	#prenom = input()
+	prenom = 'lolo la tortue'
+	print("\nlower : "+prenom.lower())
+	print("upper : "+prenom.upper())
+	print("capitalize : "+prenom.capitalize())
+	print("capitalize center : "+prenom.capitalize().center(30))
+	print("capitalize center strip : "+prenom.capitalize().center(30).strip())
+	print("Merci !")
+	
+	print("Donne moi ton age stp. ")
+	#age = input()
+	age = '2048'
+	print("Donne moi ton nom stp. ")
+	#nom = input()
+	nom = 'bienséance'
+	print("Bonjour {0} {1}, tu as {2} ans.".format(prenom, nom, age))
+	
+	print("index 3 du prenom : "+prenom[3])
+	print("index -1 du prenom : "+prenom[-1])
+	print("Ton prenom possède "+str(len(prenom)+1)+" caractères")
+	
+	print("Ton prenom de 0 à 2 : "+prenom[0:2])
+	print("Ton prenom de 2 à la longueur totale : "+prenom[2:len(prenom)])
+	print("Ton prenom du début à 4 : "+prenom[:4])
+	print("Ton prenom de 4 à la fin : "+prenom[4:])
+	print("prenom.count(\"e\") : "+str(prenom.count("e")))#compte le nombre d'occurence de la chaine passée en paramètre
+	print("prenom.replace(\"e\",\"heh\") : "+prenom.replace("e","heh"))#remplace arg1 par arg2
+	print("str(prenom.find(\"r\")) : "+str(prenom.find("r")))#Renvoie l'indice de la première occurence de la chaine passée en paramètre
+	
+	#exemple avec les listes
+	ma_liste = prenom.split(' ')#On récupère les sous chaines délimitées par les espaces
+	print(ma_liste)
+	ma_liste = '|_kamehameha_|'.join(ma_liste)#On rassemble les éléments de la liste dans une seule string
+	print(ma_liste)
+
+def listObject():
+	ma_liste0 = list() # On crée une liste vide
+	ma_liste1 = [] # On crée aussi une liste vide
+	ma_liste = [1, [1, 2.0, 'trois'], 3.2, "Salut", 'c'] # Une liste avec cinq objets
+	print(ma_liste)
+	print(*ma_liste)#Je précise à python que le paramètre est une liste d'arguments. Les paramètre sont capturés sous la forme d'un tuple
+	#Contrairement à la classe str, la classe list vous permet de remplacer un élément par un autre. Les listes sont en effet des types dits mutables.
+	ma_liste[0]='un'
+	ma_liste[1][1] = 'deux à virgule flottante'
+	print(ma_liste)
+	ma_liste.append("EOF")#Ajout d'un objet en fin de liste
+	print(ma_liste)
+	ma_liste.insert(2, 666) # On insère l'int 666 à l'indice 2, ATTENTION le tout est décalé!!!
+	print(ma_liste)
+	#Concaténation de listes
+	for i in range(0,5):
+		ma_liste0.append(i)
+	print(ma_liste0)
+	ma_liste.extend(ma_liste0)
+	print(ma_liste)
+	#Suppression d'un élément. del est une fonctionnalité de Python et remove est une méthode de list
+	del ma_liste[0]#Supprime le 1er élt de la liste
+	print(ma_liste)
+	ma_liste[1]=1
+	print(ma_liste)
+	ma_liste.remove(1)#Supprime le 1er élément qui prend la valeur entière 1
+	print(ma_liste)
+	#Boucle for pratique pour le parcours des listes. Enumerate retourne un tableau de tuples contenant indice et valeur
+	for i, elt in enumerate(ma_liste):
+		print("À l'indice {} se trouve {}.".format(i, elt))
+	#exemple des tuples
+	#À la différence des listes, les tuples, une fois créés, ne peuvent être modifiés
+	(a,b)=(1,2)#pas vraiment un tuple puisque a et b sont modifiables
+	print(a)
+	print((a,b))
+	tuple=(3,4)
+	print(tuple)
+	tuple1=4,5
+	print(tuple1)
+	tuple2=(0,)#les parenthèses et virgule permettent de spécifié que c'est un tuple. Sans la virgule, les parenthèses sont ignorées et retournera une variable lambda mais pas un tuple
+	print(tuple2)
+	#très pratique pour être utilisé en retour
+	partie_entiere, reste = decomposer(20, 3)
+	print("20/3 -> partie entière = "+str(partie_entiere)+" et reste = "+str(reste))
+	print(reste)
+	
+def decomposer(entier, divise_par):
+    """Cette fonction retourne la partie entière et le reste de
+    entier / divise_par"""
+
+    p_e = entier // divise_par
+    reste = entier % divise_par
+    return p_e, reste
 		
 def boucleWhile():
 	#Utilisation  de la boucle while
@@ -90,7 +218,15 @@ def boucleWhile():
 			continue # On retourne au while sans exécuter les autres lignes
 		print("La variable i =", i)
 		i += 1 # Dans le cas classique on ajoute juste 1 à i
-		
+	
+def boucleFor():
+	chaine = "Hello python"
+	for lettre in chaine:
+		print(lettre)
+	for i in range(1,4) :
+		print("Et "+str(i)+"!")
+
+
 def affichageSTDIN2():
 	saisie1=1
 
@@ -154,14 +290,15 @@ def exceptionTest():
 		resultat = numerateur / denominateur
 	except NameError:
 		print("La variable numerateur ou denominateur n'a pas été définie.")
+	
 	except TypeError:
 		print("La variable numerateur ou denominateur possède un type incompatible avec la division.")
-		pass#L'exception est ignorée
+		pass#L'exception est ignorée, il ne se passe rien
 	
 	except ZeroDivisionError:
 		print("La variable denominateur est égale à 0.")
 	else:
-		print("Je suis un bout de code exécuté quandaucuneexception est lancée")
+		print("Je suis un bout de code exécuté quand AUCUNE exception est lancée")
 	finally:
 		print("Je suis un bout de code exécuté  dans tous les cas")
 		
@@ -189,3 +326,207 @@ def assertionError():
 	except AssertionError:
 		print("L'année saisie est inférieure ou égale à 0.")
 		
+def exceptionRaised():
+	annee = input() # L'utilisateur saisit l'année
+	try:
+		annee = int(annee) # On tente de convertir l'année
+		if annee<=0:
+			raise ValueError("l'année saisie est négative ou nulle")
+	except ValueError:
+		print("La valeur saisie est invalide (l'année est peut-être négative).")
+		
+def functionsExamples():
+
+	for i in range(15,21):
+		print("Entre 15 et 21, ça donne : "+str(randrange(15,21)))
+		print("Et entre 15 et "+str(i+1)+" ça donne "+str(randrange(15,i+1))+"\n")
+	
+def fonction_multiparametres(*parametres):
+	"""
+	Test d'une fonction pouvant être appelée 
+	avec un nombre variable de paramètres
+	"""
+	print("J'ai reçu : {tuple}.".format(tuple = parametres))
+	print("J'ai reçu : {}.".format(parametres))
+	
+
+def fonction_multiparametres_avec_2_param_mini(nom, prenom, *commentaires):
+	print("{Nom} {Prenom}".format(Nom = nom.upper(), Prenom = prenom))
+	print("J'ai reçu : {tuple}.".format(tuple = commentaires))
+
+def fonction_multiparametres_full(arg0, *param, pays='France', planete='Terre'):
+	#Les paramètres nommés peuvent aussi être mis dans un dico
+	print("J'ai reçu : {tuple}.".format(tuple = arg0))
+	print("J'ai reçu : {tuple}.".format(tuple = param))
+	
+	param = list(param)
+	print("{} ".format(len(param))+"paramètres ont été ajoutés")
+	if pays =='France' and planete=='Terre':
+		print("Les param par défaut n'ont pas été modifiés")
+	else:
+		print("Pays : {0}\nPlanète : {1}".format(pays.capitalize(), planete.capitalize()))
+
+def comprehension_de_liste():
+	#forme générale : [<expression> for <tuple> in <list> if <condition>]
+	#explication :
+	#	-	expression permet de calculer la valeur à mettre dans la liste résultante à la valeur correspondante. L'expression est appliquée au tuple
+	#	-	tuple est un élément de la liste parcourue
+	#	-	list est la liste d'origine sur laquelle la transformation et le filtrage sont effectués
+	# 	-	condition porte sur l'élément de la liste parcourue. Si la condition est validée le tuple est ajouté à la liste résultante
+	
+	liste_origine = [0, 1, 2, 3, 4, 5]
+	print("liste_origine : ",*liste_origine)
+	liste_au_carre = [nb * nb for nb in liste_origine]
+	print("liste_au_carre : ",*liste_au_carre)
+	pairs = [nb for nb in liste_au_carre if nb % 2 == 0]
+	print("pairs : ",*pairs)
+	tierce = [nb for nb in liste_au_carre if nb % 3 == 0]
+	print("tierce : ",*tierce)
+	
+	modkat = [nb for nb in liste_au_carre if nb % 4 == 0]
+	print("modulo 4 : ",*modkat)
+	
+	liste_de_personnes=[\
+	(12,'a','g'),\
+	(14,'m','t'),\
+	(18,'h','h'),\
+	(20,'w','v'),\
+	]
+	print(*liste_de_personnes)
+	print(*[pers for pers in sorted(liste_de_personnes, reverse=True)])
+	#modification de la liste
+	liste_de_personnes = [(nom, prenom, age) for age, nom, prenom in liste_de_personnes]
+	print(*liste_de_personnes)
+	print(*[pers for pers in sorted(liste_de_personnes, reverse=True)])#sorted() tri la liste suivant le premier élément
+	#Pour avoir le même résultat que précédemment mais à partir de ma liste modifiée
+	#Problématique : trier la liste de tuple (nom, prenom, age) dans l'ordre décroissant des âges
+	# 1 ) On récupère la liste de personnes avec l'âge en premier
+	# 2 ) On tri la liste récupérée
+	# 2.bis ) En sens inverse
+	# 3) Je spécifie que cette liste doit être itérée
+	# 4 ) On récupère les tuples sous une certaine forme. Précise la forme, j'aurai très bien pu mettre "pers" ou "var", ... si je voulais garder la même forme
+	# 5 ) On spécifie le nouvel ordre des éléments des tuples
+	#Je précise que la liste qui suit est une liste de paramètres
+	print(*[(nom, prenom, age) for age, nom, prenom in  sorted([(age, nom, prenom) for nom, prenom, age in liste_de_personnes], reverse=True)])
+
+def dico():
+	dico = dict()
+	dico1 = {}
+	#dico = { key1:value1, key2:value2, ..., keyN:valueN }
+	#dico[key]=value
+	#
+	#key : tuple unique pointant vers une valeur
+	#value : variable lambda dont le contenu est accessible via une clé
+	dico[(1,1)]='un'
+	dico[(1,2)]='deux'
+	dico[(2,1)]='deux bis'
+	dico[(2,2)]='chat'
+	try:
+		print(dico[(1,)])
+	except:
+		print('Tu essaies d\'accéder à une donnée qui n\'existe pas')
+	placard = {"chemise":3, "pantalon":6, "tee-shirt":7}
+	print('Ton placard contient : ',placard)
+	print('Suppression de tes chemises')
+	print('Je te prends les',placard.pop('chemise'))
+	print('Ton placard contient : ',placard)
+	#Parcours d'un dictionnaire
+	for key in placard.keys() :
+		print(key)
+	for values in placard.values() :
+		print(values)
+	for key, value in placard.items() :
+		print('{0} {1}'.format(value, key))
+	#Conditionnel avec les dico
+	if 7 in placard.values():
+		print('Tu as 7 instances d\'un certain vetement')
+	
+def fonction_multi_parametres_nommes(**params_nommes):
+	print("paramètres reçus : {}.".format(params_nommes))
+
+def fonction_jmenfoutiste(*liste, **dico):
+	print('Voici les paramètres que tu m\'as transmis.\nC\'est gentil mais je n\'en ferai rien...')
+	for key, value in enumerate(liste):
+		print('{0} : {1}'.format(key, value),'(type :',type(value),')')
+	for key, value in dico.items() :
+		print('{0} : {1}'.format(key, value),'(type :',type(value),')')
+
+def dico_de_fonctions():
+	mes_fonctions = dict()
+	mes_fonctions['salut'] = salut
+	mes_fonctions['au revoir'] = bye
+	print('Adresse de ma fonction salut : \n\t\t',mes_fonctions['salut'])
+	mes_fonctions['salut']()
+	mes_fonctions['au revoir']()
+
+def salut():
+	print('salut')
+	
+def bye():
+	print('hasto luego bimbo')
+
+	
+def set_examples():
+	#Un set ne peut pas contenir de doublons. Sinon c'est comme une liste.
+	mon_set = {'salut', 'set'}
+	
+def fichiers():
+	os.chdir("C:/workspaces/Python/tests")#Par défaut le répertoire est C:/Python34/. Mon répertoire est C:/workspaces/Python/tests, je peux utiliser le chemin relatif
+	#mon_fichier = open("hellopython.py", "r")
+	#contenu = mon_fichier.read()
+	#print('Contenu du fichier\n',contenu)
+	#mon_fichier.close()
+	
+	write(*["log/log.txt", "w"], \
+		value = 'Argh j\'ai tout écrasé ! x	fois')
+	read(*["log/log.txt", "r"])
+	write(*["log/log.txt", "a"], \
+		value = '\nCette fois-ci je n\'ai pas écrasé')
+	read(*["log/log.txt", "r"])
+
+def write(*param, value='default value'):
+	#mon_fichier = open(*param)
+	with open(*param) as mon_fichier:#Meilleure syntaxe, Python gère seul la fermeture du fichier. Qu'il y ai ou pas d'execption
+		contenu = mon_fichier.write(value)
+	#mon_fichier.close()
+	
+def read(*param):
+	#mon_fichier2 = open(*param)
+	with open(*param) as mon_fichier:
+		contenu = mon_fichier.read()
+		print(contenu)
+	#mon_fichier2.close()
+	
+def sauvegarde_objet():
+	score = {
+		"joueur 1":    5,
+		"joueur 2":   35,
+		"joueur 3":   20,
+		"joueur 4":    2,
+	}
+		
+	
+	save_objet(score)
+	print('L\'objet est correctement enregistré')
+	recup_objet()
+
+def save_objet(score):
+	with open('donnees.bin', 'wb') as fichier:
+		mon_pickler = pickle.Pickler(fichier)
+		mon_pickler.dump(score)
+
+def recup_objet():
+	"""
+	docstring
+	"""
+	#global i
+	global j
+	with open('donnees.bin', 'rb') as fichier:
+		mon_depickler = pickle.Unpickler(fichier)
+		score_recupere = mon_depickler.load()
+		print(*score_recupere)
+		
+		print(i)
+		print(j)
+
+#
